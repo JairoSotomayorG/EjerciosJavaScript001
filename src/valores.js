acebca85bf9853052f1aa385f2a72b8cf14e2dfc
JairoSotomayorG <116982364+JairoSotomayorG@users.noreply.github.com>
2023-01-29 10:59:18 -0500

    commit valores

---
+++ src/valores.js
@@ -0,0 +1,21 @@
+const nombre = "jairo Sotomayor";
+
+const edad = 37;
+
+var elemento = ["celaura", "telefono"];
+
+// funciones declarativas
+
+function miFuncion(){
+    return 3;
+
+}
+
+
+//funcion expresiva
+
+var mifuncion = function(a,b){
+    return a + b;
+}
+
+miFuncion();
