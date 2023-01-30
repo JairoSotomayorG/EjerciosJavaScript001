3db710b07e864ddca52c1b6b174fb0eb840a3579
JairoSotomayorG <116982364+JairoSotomayorG@users.noreply.github.com>
2023-01-30 16:58:10 -0500

    commit: string , defaul params

---
+++ src/es6/03 -string.js
@@ -0,0 +1,26 @@
+let hello = 'Hello';
+let world = 'World';
+
+// antigua forma de concatenar
+let epicPhrase = hello + ' ' + world + '!';
+console.log(epicPhrase);
+
+// Template literals  :: nueva forma de concatenar
+let epicPhrase2 = `${hello} ${world}!`;
+console. log(epicPhrase2)
+
+
+//multiline string
+
+// antigua forma
+let lorem = 'esto es un string \n ' + 'esto es otra liena';
+
+//nueva forma
+let lorem2 = `Esta es una frase epica
+  la continuacion de esa frase epica.
+`;
+
+
+console.log(lorem);
+console.log(lorem2);
+
