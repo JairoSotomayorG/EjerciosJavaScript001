974ddc77d1d06fdaa277c3e98cee475c3afdb858
JairoSotomayorG <116982364+JairoSotomayorG@users.noreply.github.com>
2023-01-30 15:00:12 -0500

    commit var, ñet, const, arrow function

---
+++ src/es6/00-let-const.js
@@ -0,0 +1,20 @@
+//  uso de let y const
+
+// declarar y reasignar var
+var lastNAme = 'david';
+lastNAme = 'Oscar';
+console.log(lastNAme);
+
+
+// declarar y reasignar let
+let fruit = 'apple';
+fruit = 'Kiwi';
+console.log(fruit);
+
+
+// declarar y reasignar con const ::  OJO no se puede 
+//porque no se puede reasigar con const.
+
+cont animal = 'dog';
+animal = 'cat';
+console.log(animal);
---
+++ src/es6/001-arrow- function.js
@@ -0,0 +1,15 @@
+// forma antigua
+
+function square(num) {
+    return num * num;
+}
+
+
+// forma de arrow function  :: cuando tiene mas de una linea, hay que poner llaves y return
+const square = (num) => {
+    return num * num;
+}
+
+
+// forma de arrow function  :: solo cuando hay una linea.
+const square = num => num * num;
